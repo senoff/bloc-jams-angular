938acba8abe286e307739af582a02b34b9497679
Bob Senoff <bobsenoff@gmail.com>
2017-01-11 12:19:04 -0800

    Angular-2 checkpoint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,32 @@
 module.exports = function(grunt) {
-
-    grunt.registerTask( 'default', [ 'clean', 'copy', 'hapi', 'watch'] );
+    
+    grunt.registerTask('default', 'Testing out Grunt logging and task callbacks', ['watch']);
+    
+    grunt.registerTask('fun', 'This task is for fun only', function() {
+        grunt.log.writeln('This the *fun* Grunt task');
+    });
+ 
+    grunt.registerTask('serious', 'This task is for serious stuff only', function() {
+        grunt.log.writeln('Wipe that smirk off your face; this is serious.');
+    });
+    
+     
+    grunt.initConfig({
+        watch: {
+            files: ['Gruntfile.js']
+        },
+ 
+        copy: {
+            main: {
+                files: {
+                    './dest/': ['assets/js/*.js']
+                }
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.registerTask( 'build', [ 'clean', 'copy' ] );
 
