116b9301e78c15af21971764f4f9d4a43f374c53
Bob Senoff <bobsenoff@gmail.com>
2017-02-02 11:34:21 -0800

    angular-6-assignment stuff

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -37,8 +37,22 @@
             return albumPicasso;
         };
 
+
+
+        Fixtures.getCollection = function(numberOfAlbums) {
+            this.albums = [];
+            for (var i = 0; i < numberOfAlbums; i++) {
+                this.albums.push(angular.copy(albumPicasso));
+            }
+
+            return this.albums;
+        };
+
         return Fixtures;
+
     }
+
+
 
     angular
         .module('blocJams')
