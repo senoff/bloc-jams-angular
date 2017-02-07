d814d13590585d946b0b545582861f5cf477e542
Bob Senoff <bobsenoff@gmail.com>
2017-02-06 19:01:59 -0800

    let add in the /dist files

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,13 +2,72 @@
     function SongPlayer() {
         var SongPlayer = {};
 
-        SongPlayer.play = function(song) {
-            var currentBuzzObject = new buzz.sound(song.audioUrl, {
+        /**
+        * @desc Buzz object audio file
+        * @type {object}
+        */
+
+        var currentSong = null;
+        var currentBuzzObject = null;
+
+
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {object} song
+        */
+
+        var setSong = function(song) {
+            if (currentBuzzObject){
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
 
+            currentSong = song;
+        };
+
+
+        /**
+        * @function playSong
+        * @desc Starts playing song and sets the playing state to true
+        */
+
+        var playSong = function(){
             currentBuzzObject.play();
+            song.playing = true;
+        }
+
+        /**
+        * @function Songplayer.play
+        * @desc Called through Album.html on the play button ng-click and then through AlbumCtrl
+        * @param {object} song
+        */
+
+        SongPlayer.play = function(song) {
+            if (currentSong !== song) {
+                setSong(song);
+                playSong();
+            } else if (currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    currentBuzzObject.play();
+                }
+            }
+        };
+
+        /**
+        * @function SongPlayer.pause
+        * @desc Called through Album.html on the pause button ng-click
+        * @param {object} song
+        */
+
+        SongPlayer.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
         };
 
         return SongPlayer;
